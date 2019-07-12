c506ea0b7e24ad8c80450a34e5f34f532d9a828e
Matt Deason <d.sonmd@gmail.com>
2019-07-12 18:16:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(array, next_color)
+  colors_in_the_rainbow = [array]
+  array.push(next_color)
+end
+
+def using_unshift(array, new_neighborhood)
+  bouroughs_in_nyc = [array]
+  array.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  continents = array.pop
+end
+
+def pop_with_args(array)
+  dog_breeds = array.pop(2)
+end
+
+def using_shift(array)
+  my_favorite_cities = array.shift 
+end
+
+def shift_with_args(array)
+  ice_cream_brands = array.shift(2)
+end
+
+def using_concat(array, more_favs)
+  array.concat(more_favs)
+end
+
+def using_insert(array, another_language)
+  array.insert(4)
+  
+end  
+  
